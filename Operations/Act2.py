67674d486400d755f3aa2717ee4d314d9fdd08e5
Tupm3 <jediulises@yahoo.com.mx>
2020-03-05 21:02:12 -0600

    Updates

--- Operations/Act2.py
+++ Operations/Act2.py
@@ -2,7 +2,8 @@ import matplotlib as p
 from Operations.MathOp import MathOp
 from math import sqrt
 import os
-class Act2(MathOP):
+from DB import *
+class Act2(MathOp):
     ''' # MathOpExtension\n
     - Clase con Métodos para la Actividad 2 del Experimento\n
     Notas:\n
@@ -11,6 +12,26 @@ class Act2(MathOP):
     Ain = 0
     Ah = 0
     g =9.81
+    tiempos = []
+    cmA2 = []
+    Hagua = []
+    hPopote = []
+
+    @staticmethod
+    def import_Data():
+        path = os.getcwd()
+        resultPath = path+"\DB"
+        os.chdir(resultPath)
+        with open("tiemposA2.txt","r") as f: 
+            for element in f.readlines(): tiempos.append(float(element))
+        with open("cmA2.txt","r") as f: 
+            for element in f.readlines(): cmA2.append(float(element))
+        with open("Hagua.txt","r") as f: 
+            for element in f.readlines(): Hagua.append(float(element))
+        with open("hPopote.txt","r") as f: 
+            for element in f.readlines(): hPopote.append(float(element))
+
+
     @staticmethod
     def minimos_cuadrados(lx,ly):
         '''# Minimos Cuadrados\n
@@ -51,15 +72,52 @@ class Act2(MathOP):
         return minimosCuadrados
 
         @staticmethod
+        def B0(x,y,B1):
+            '''# Beta 0\n
+            Encuentra la variable con la forma:\n
+            B0 = promY - (Beta1 * promX)'''
+            py = super().promedio(y)
+            '''Promedio de Y'''
+            px = super().promedio(x)
+            '''Promedio de X'''
+            B1 = minimosCuadrados(x,y)
+            '''Beta 1 con minimos cuadrados'''
+            b0 = py - (B1*px)
+            '''Aplicacion de la formula'''
+            '''Regresa el calculo'''
+            return b0
+
+        @staticmethod
         def plot_graph(lx,ly,graph,xName = "X",yName = "Y"):
-            p.plot(lx,ly)
+            '''# Plot Graph\n
+            Metodo invocable para graficar datos y guardarlos'''
             p.xlabel(xName)
+            '''Nombre del eje X'''
             p.ylabel(yName)
+            '''Nombre del eje Y'''
+            p.plot(lx,ly)
+            '''Grafica'''
             name = "Grafica_{}.png".format(graph)
             p.savefig(name)
+            '''Exportacion de la grafica'''
             print("Gráfica "+name+" exportada...")
         
         @staticmethod
-        def Vh(H):
-            Vh = -(Aout/Ah) * sqrt(2*g*H)
-        return Vh
+        def Vh():
+            aIndex = 0
+            fIndex = 10
+            rep = 0
+            for i in range(0,10):
+                H = hPopote[rep]
+                b1 = minimos_cuadrados(tiempos[aIndex:fIndex],Hagua)
+                b0 = B0(tiempos[aIndex:fIndex],Hagua,b1)
+                Vh = b0 + b1*H
+                aIndex += 10
+                fIndex += 10
+                rep += 1
+                print(Vh)
+                print("-"*57)
+            print("**END**")
+
+Vh()
+
