be4c79ca6adf7303821d05ae5aab7d127ba344ca
Tupm3 <jediulises@yahoo.com.mx>
2020-03-10 14:27:12 -0600

    Updates

--- Operations/Act2.py
+++ Operations/Act2.py
@@ -11,7 +11,7 @@ class Act2(MathOp):
     - Hereda de MathOp'''
     Aout = 0.1257
     Ain = 0
-    Ah = 143 - 0.2827
+    Ah = 142.7173
     g =9.81
     tiempos = []
     cmA2 = []
@@ -135,25 +135,26 @@ class Act2(MathOp):
 
     
     @staticmethod
-    def sy(N,ly):
-        sx = 0
-        sx2 = 0
+    def sy(N,ly,est):
+        sys = 0
+        index = 0
         for element in ly:
-            sx += element
-            sx2 += element * element
-        denum = (N*sx2) - (sx*sx)
-        result = sqrt(N/denum)
+            item = element-est[index]
+            sys+= item * item
+            index += 1
+        result = sqrt(sys/(N-2))
         return result
     
     @staticmethod
-    def incertidumbre(N,lx,ly):
-        sy = Act2.sy(N,ly)
+    def incertidumbre(N,lx,ly,est):
+        sy = Act2.sy(N,ly,est)
         sx2 =0
         sx = 0
         for element in lx:
             sx+=element
             sx2 += element * element
         denum = (N*sx2) - (sx*sx)
+        #r = sqrt(N/denum)
         r = sqrt(sx2/denum)
         result = sy * r
         return result
@@ -184,7 +185,6 @@ class Act2(MathOp):
             b0 = Act2.B0(times,Act2.Hagua[aIndex:fIndex],b1)
             print("B0: "+str(b0))
             print("B1: "+str(b1))
-            dVhlist.append(Act2.incertidumbre(len(times),times,Act2.Hagua[aIndex:fIndex]))
             print("Error de Vh: ",dVhlist)
             for c in range(0,10):
                 hList.append(b0+(b1*times[c]))
@@ -196,6 +196,7 @@ class Act2(MathOp):
             Act2.plot_graph(times,Act2.Hagua[aIndex:fIndex],graphName,"Tiempos","Alturas",False)
             nx,ny = Act2.lineaAjuste(times[len(times)-1],b1,b0)
             Act2.plot_graph(nx,ny,graphName,"Tiempos","Alturas")
+            dVhlist.append(Act2.incertidumbre(len(times),times,Act2.Hagua[aIndex:fIndex],ny))
             print("-"*57)
             aIndex += 10
             fIndex += 10
@@ -213,7 +214,6 @@ class Act2(MathOp):
             dVh2list.append(2*element*dVhlist[index])
             index+=1
         b1 = Act2.minimos_cuadrados(Act2.hPopote,squaredVh)
-        db1 = Act2.incertidumbre(len(Act2.hPopote),Act2.hPopote,squaredVh)
         b0 = Act2.B0(Act2.hPopote,squaredVh,b1)
         print("B0: ",b0)
         print("B1: ",b1)
@@ -221,6 +221,7 @@ class Act2(MathOp):
         nx2,ny2 = Act2.lineaAjuste(Act2.hPopote[len(Act2.hPopote)-1],b1,b0,Act2.hPopote[0])
         Act2.plot_graph(nx2,ny2,"Vh2","H","Vh^2")
         g = (b1 * (Act2.Ah * Act2.Ah))/(2 * (Act2.Aout*Act2.Aout))
+        db1 = Act2.incertidumbre(len(Act2.hPopote),Act2.hPopote,squaredVh,ny2)
         print("-"*57)
         print("Gravedad: ",g)
         print("="*57)
@@ -250,3 +251,14 @@ class Act2(MathOp):
         dAout = 0.1257
         dg = (1/2) * ((2*Act2.Ah*dAout*b1*pow(Act2.Aout,-2)) + (db1*(Act2.Ah * Act2.Ah)* pow(Act2.Aout,-2)) + (2*pow(Act2.Aout,-3)*dAout*b1*(Act2.Ah * Act2.Ah)))
         print("El error de la gravedad es: ",dg)
+        print("La wea con su error es: "+str(g+dg))
+        print("VH")
+        print("="*57)
+        for element in vhlist:
+            print(element)
+        print("-"*57)
+        print("Vh^2")
+        print("="*57)
+        for element in squaredVh:
+            print(element)
+        print("-"*57)
