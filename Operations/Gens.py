bf9ef030743c589ea3ea49a02042b33fa4ed3ec1
Tupm3 <44625748+Tupm3@users.noreply.github.com>
2020-03-13 13:11:45 -0600

    Update

--- Operations/Gens.py
+++ Operations/Gens.py
@@ -8,11 +8,6 @@ class Gens:
     '''# Clase de Generadores '''
     @staticmethod
     def generateData(data, num):
-        '''# Generate Data\n
-        Metodo invocable para generar datos con una vaiacion pequeña e.e\n
-        Requiere:
-        - El dato base
-        - Numero de datos que va a generar la lista '''
         lista = []
         for i in range(0,num):
             lista.append(float(random.uniform(data+0.1,data+0.6)))
